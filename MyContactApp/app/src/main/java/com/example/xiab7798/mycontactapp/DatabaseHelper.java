442e75cffc5dd16f258dfcca852c02090f791974
bxia4120 <bxia@mit.edu>
2017-05-15 09:26:57 -0700

    Changed User Interface and added more fields (name, age, email, phone)

--- MyContactApp/app/src/main/java/com/example/xiab7798/mycontactapp/DatabaseHelper.java
+++ MyContactApp/app/src/main/java/com/example/xiab7798/mycontactapp/DatabaseHelper.java
@@ -4,6 +4,7 @@ package com.example.xiab7798.mycontactapp;
  * Created by xiab7798 on 5/12/2017.
  */
 
+import android.content.ContentValues;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.database.sqlite.SQLiteDatabase;
 import android.content.Context;
@@ -15,19 +16,38 @@ public class DatabaseHelper extends SQLiteOpenHelper{
     public static final String DATABASE_NAME = "Contact.db";
     public static final String TABLE_NAME = "contact_table";
     public static final String COL_1 = "ID";
-    public static final String COL_2 = "NAME";
+    public static final String COL_2A = "NAME";
+    public static final String COL_2B = "AGE";
+    public static final String COL_2C = "EMAIL";
+    public static final String COL_2D = "PHONE";
 
-    public DatabaseHelper (Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+
+    public DatabaseHelper (Context context) {
         super (context, DATABASE_NAME, null, 1);
     }
 
     @Override
     public void onCreate (SQLiteDatabase db) {
-
+        db.execSQL("CREATE TABLE " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT) ");
     }
 
     @Override
     public void onUpgrade (SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
 
+    public boolean insertData(String name, String age, String email, String phone) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_2A, name);
+        contentValues.put(COL_2B, age);
+        contentValues.put(COL_2C, email);
+        contentValues.put(COL_2D, phone);
+
+        long result = db.insert(TABLE_NAME, null, contentValues);
+
+        if(result==-1) return true;
+        else return false;
     }
 }
--- MyContactApp/app/src/main/java/com/example/xiab7798/mycontactapp/MainActivity.java
+++ MyContactApp/app/src/main/java/com/example/xiab7798/mycontactapp/MainActivity.java
@@ -2,12 +2,41 @@ package com.example.xiab7798.mycontactapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    DatabaseHelper myDb;
+    EditText editName;
+    EditText editAge;
+    EditText editEmail;
+    EditText editPhone;
+    Button btnAddData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myDb = new DatabaseHelper (this);
+    }
+
+    public void addData (View v) {
+        boolean isInserted = myDb.insertData(editName.getText().toString(), editAge.getText().toString(), editEmail.getText().toString(), editPhone.getText().toString());
+        if (isInserted==true) {
+            Log.d("myContact", "Data insertion successful");
+            //create toast message to user indicating data inserted correctly
+            Toast.makeText(getApplicationContext(), "Success!", Toast.LENGTH_SHORT).show();
+        }
+
+        else {
+            Log.d("myContact", "Data insertion NOT successful");
+            //create toast message to user indicating data inserted incorrectly
+            Toast.makeText(getApplicationContext(), "NOT Success!", Toast.LENGTH_SHORT).show();
+        }
     }
 }
